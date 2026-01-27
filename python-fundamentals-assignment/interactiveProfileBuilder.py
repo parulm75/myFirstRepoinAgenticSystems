28a21297f882a2a561beb41e2f9cae29146fa17f
parulm75 <parulm75@my.yorku.ca>
2026-01-26 20:48:39 -0500

    InteractiveProfileBuilder file created

---
+++ python-fundamentals-assignment/interactiveProfileBuilder.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+validateActiveUser=input("Are you an active user(True/False): ")
+print(f"User {name} is {age} years old. Active status: {validateActiveUser}")

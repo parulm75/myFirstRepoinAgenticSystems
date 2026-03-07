216d202062353e935049078ff5f8803cbef619d7
parulm75 <parulm75@my.yorku.ca>
2026-03-07 15:06:37 -0500

    Add SQL query assignment - students table

---
+++ sql-query-assignment/queries.sql
@@ -0,0 +1,47 @@
+
+SELECT *
+FROM students;
+
+SELECT name,score
+FROM students;
+
+SELECT *
+FROM students
+WHERE score>80;
+
+SELECT *
+FROM students
+WHERE score>80 AND passed=1;
+
+SELECT *
+FROM students
+WHERE score>80 AND class=9;
+
+SELECT name
+FROM students
+WHERE  passed=0;
+
+SELECT DISTINCT class
+FROM students;
+
+-- SELECT *
+-- FROM students
+-- ORDER BY score DESC
+-- LIMIT 5;
+
+SELECT *
+FROM students
+ORDER BY calss,score DESC;
+
+SELECT name,score as Final_score
+FROM students;
+
+SELECT name,score
+FROM students
+WHERE score>75
+ORDER BY score DESC
+LIMIT 3;
+
+
+
+
